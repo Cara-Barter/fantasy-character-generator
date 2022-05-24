4fb467f9906f3a49e75a7f64437093fa9f80e48c
Cara Barter <carabarter@gmail.com>
2022-05-24 15:14:58 -0700

    cleaned up code

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -1,12 +1,12 @@
 import './Characters.scss';
 
-function Characters ({ input, reply, list }) {
+function Characters ({ input, reply }) {
     return (
        <section className="characters">
            <h2 className="characters__title">Characters</h2>
            <ul className="characters__list">
-               {list.map(item => (
-                <li className="characters__item" key={item}>
+              
+                <li className="characters__item">
                     <div className="characters__box">
                         <span className="characters__header">Fantasy Race:</span>
                         <p className="characters__text">{input}</p>
@@ -17,8 +17,6 @@ function Characters ({ input, reply, list }) {
                     </div>
                    
                </li>
-               ))}
-                
            </ul>
            
        </section>
